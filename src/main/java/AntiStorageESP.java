3451940e32c37840efb34ceced0fa5bb326e5af2
ProVlad14 <vladflav1401@gmail.com>
2026-06-30 21:03:27 +0300

    Update AntiStorageESP.java

--- src/main/java/AntiStorageESP.java
+++ src/main/java/AntiStorageESP.java
@@ -1,6 +1,5 @@
 import com.github.retrooper.packetevents.PacketEvents;
-import com.github.retrooper.packetevents.event.PacketListenerPriority;
-import com.github.retrooper.packetevents.event.SimplePacketListenerAbstract;
+import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerBlockEntityData;
@@ -9,7 +8,7 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AntiStorageESP extends JavaPlugin {
+public final class AntiStorageESP extends JavaPlugin implements PacketListener {
     private final double maxEspDistanceSquared = 16.0 * 16.0; 
 
     @Override
@@ -21,31 +20,29 @@ public final class AntiStorageESP extends JavaPlugin {
     @Override
     public void onEnable() {
         PacketEvents.getAPI().init();
-        PacketEvents.getAPI().getEventManager().registerListener(
-            new SimplePacketListenerAbstract(PacketListenerPriority.NORMAL) {
-                @Override
-                public void onPacketSend(PacketSendEvent event) {
-                    Player player = (Player) event.getPlayer();
-                    if (player == null) return;
-
-                    if (event.getPacketType() == PacketType.Play.Server.BLOCK_ENTITY_DATA) {
-                        WrapperPlayServerBlockEntityData packet = new WrapperPlayServerBlockEntityData(event);
-                        com.github.retrooper.packetevents.util.Vector3i vec = packet.getBlockPosition();
-                        
-                        Location playerLoc = player.getLocation();
-                        double distSq = playerLoc.distanceSquared(new Location(player.getWorld(), vec.getX(), vec.getY(), vec.getZ()));
-                        
-                        if (distSq > maxEspDistanceSquared) {
-                            event.setCancelled(true);
-                        }
-                    }
-                }
-            }
-        );
+        PacketEvents.getAPI().getEventManager().registerListener(this);
     }
 
     @Override
     public void onDisable() {
         PacketEvents.getAPI().terminate();
+    }
+
+    @Override
+    public void onPacketSend(PacketSendEvent event) {
+        Player player = (Player) event.getPlayer();
+        if (player == null) return;
+
+        if (event.getPacketType() == PacketType.Play.Server.BLOCK_ENTITY_DATA) {
+            WrapperPlayServerBlockEntityData packet = new WrapperPlayServerBlockEntityData(event);
+            com.github.retrooper.packetevents.util.Vector3i vec = packet.getBlockUserPos();
+            
+            Location playerLoc = player.getLocation();
+            double distSq = playerLoc.distanceSquared(new Location(player.getWorld(), vec.getX(), vec.getY(), vec.getZ()));
+            
+            if (distSq > maxEspDistanceSquared) {
+                event.setCancelled(true);
+            }
+        }
     }
 }
